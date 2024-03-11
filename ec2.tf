09df8acbc1235f70e0c4881c46c3a33a8fc29de9
Giovanni Lima <83291215+giovannipajeu1@users.noreply.github.com>
2024-03-10 23:47:00 -0300

    Add files via upload

--- ec2.tf
+++ ec2.tf
@@ -16,9 +16,26 @@ resource "aws_instance" "ec2_web" {
     name = "Ec2 da Aplicacao Python"
   }
   lifecycle {
-    prevent_destroy = false
+    prevent_destroy = true
   }
 }
+resource "aws_instance" "ec2_web_redun" {
+
+  ami                         = var.ami
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.avg_subnet_pub.id
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.avg_sg_web.id]
+  key_name                    = aws_key_pair.aws_key_pair.key_name
+
+  tags = {
+    name = "Ec2 Redundancia"
+  }
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
 
 
 resource "aws_instance" "ec2_data-base" {
@@ -33,6 +50,38 @@ resource "aws_instance" "ec2_data-base" {
     name = "Ec2 do  MongoDB"
   }
   lifecycle {
-    prevent_destroy = false
+    prevent_destroy = true
+  }
+}
+
+resource "aws_instance" "ec2_back-up" {
+
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.avg_subnet_priv.id
+  vpc_security_group_ids = [aws_security_group.avg_sg_db.id]
+  key_name               = aws_key_pair.aws_key_pair.key_name
+
+  tags = {
+    name = "Ec2 de back-up"
+  }
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_instance" "ec2_mysql" {
+
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.avg_subnet_priv.id
+  vpc_security_group_ids = [aws_security_group.avg_sg_db.id]
+  key_name               = aws_key_pair.aws_key_pair.key_name
+
+  tags = {
+    name = "Ec2 mysql"
+  }
+  lifecycle {
+    prevent_destroy = true
   }
 }
--- variables.tf
+++ variables.tf
@@ -11,43 +11,43 @@ variable "secret_key_aws" {
 }
 
 variable "region" {
-  type = string
+  type    = string
   default = "us-east-1"
-  
+
 }
 
 variable "port_data_base" {
-  type = number
+  type    = number
   default = 27017
 }
 
 
 variable "instance_tenancy" {
-  type = string
+  type    = string
   default = "default"
 }
 
 variable "instance_type" {
-  type = string
+  type    = string
   default = "t2.micro"
 }
 
 variable "ami" {
-  type = string
+  type    = string
   default = "ami-0f403e3180720dd7e"
 }
 
 variable "cidr_block_vpc" {
-  type = string
+  type    = string
   default = "10.0.0.0/16"
 }
 
 variable "cidr_block_subnet_pub" {
-  type = string
+  type    = string
   default = "10.0.0.0/24"
 }
 
 variable "cidr_block_subnet_priv" {
-  type = string
+  type    = string
   default = "10.0.1.0/24"
 }
